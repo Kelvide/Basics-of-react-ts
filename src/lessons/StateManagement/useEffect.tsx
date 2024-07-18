376c3a9e43c47aeacaa494345f099f8b3773c9b5
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-07-17 20:20:20 -0700

    Update useEffect.tsx

--- src/lessons/StateManagement/useEffect.tsx
+++ src/lessons/StateManagement/useEffect.tsx
@@ -9,8 +9,31 @@ const Component = () => {
     }, [])
 
 
+    const [time, setTime] = useState(10)
+
+    useEffect(() => {
+        // This approach continuously calls setTime with a fixed 'time' value from the initial render.
+        // This can lead to incorrect updates due to stale state, as 'time' may not reflect the latest state value.
+        // setInterval(() => {
+        //     setTime(time + 1)
+        // }, 1000)
+
+        // Second Approach (Recommended)
+        // This approach uses the functional form of setState, which ensures that the state update is based on the latest state value, preventing issues related to stale state.
+        // This is particularly important in React to ensure accurate and reliable state updates.
+        const interval = setInterval(() => {
+            setTime((time) => time + 1)
+        }, 1000)
+        return () => clearInterval(interval);
+    }, [])
+
+
+
     return (
-        <div>Names: {names.join(', ')}</div>
+        <>
+            <p>Names: {names.join(', ')}</p>
+            <p>Time: {time}</p>
+        </>
     )
 }
 
